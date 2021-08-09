fc94feb93f793189a4e91b0a9c03b210732a4523
jankeesvanandel <jankeesvanandel@gmail.com>
2021-08-09 15:52:06 +0200

    added prettier and husky

--- src/receiver.ts
+++ src/receiver.ts
@@ -1,27 +1,27 @@
-import {Receiver} from "@streammachine.io/nodejs-driver";
+import { Receiver } from '@streammachine.io/nodejs-driver';
 
-const CONFIG = require("../assets/config.json");
-const CREDENTIALS = require("../assets/credentials.json");
+const CONFIG = require('../assets/config.json');
+const CREDENTIALS = require('../assets/credentials.json');
 
 async function startReceiver() {
-    const receiver = new Receiver({
-        ...CREDENTIALS,
-        ...CONFIG
-    });
+  const receiver = new Receiver({
+    ...CREDENTIALS,
+    ...CONFIG,
+  });
 
-    receiver.on("event", (event) => {
-        console.log(JSON.stringify(event));
-    });
+  receiver.on('event', (event) => {
+    console.log(JSON.stringify(event));
+  });
 
-    receiver.on("error", (error) => {
-        console.log("Error", error);
-    });
+  receiver.on('error', (error) => {
+    console.log('Error', error);
+  });
 
-    try {
-        await receiver.connect();
-    } catch (error) {
-        console.log("Connect failed", error);
-    }
+  try {
+    await receiver.connect();
+  } catch (error) {
+    console.log('Connect failed', error);
+  }
 }
 
 startReceiver();
--- src/sender.ts
+++ src/sender.ts
@@ -1,63 +1,54 @@
-import { Sender } from "@streammachine.io/nodejs-driver";
-import { ClickstreamEvent } from "@streammachine.io/schemas-clickstream-avro";
-import { randomInt } from "crypto";
+import { Sender } from '@streammachine.io/nodejs-driver';
+import { DemoEvent } from '@streammachine.io/schemas-demo-avro';
 
-const CONFIG = require("../assets/config.json");
+const CONFIG = require('../assets/config.json');
 
 // Copy the template credentials file, name it `credentials.json` and fill out the values
-const CREDENTIALS = require("../assets/credentials.json");
+const CREDENTIALS = require('../assets/credentials.json');
 
 const startSender = async () => {
-    // Note: the schema id is hard coded in this example, in the config.json. This will be dynamically determined in a future version
-    const sender = new Sender({
-        ...CONFIG,
-        ...CREDENTIALS
-    });
+  // Note: the schema id is hard coded in this example, in the config.json. This will be dynamically determined in a future version
+  const sender = new Sender({
+    ...CONFIG,
+    ...CREDENTIALS,
+  });
 
-    // Make sure to listen for error events, otherwise Node does not handle the error events (they're escalated)
-    sender.on("error", (error) => {
-        console.log("Sender", error);
-    });
+  // Make sure to listen for error events, otherwise Node does not handle the error events (they're escalated)
+  sender.on('error', (error) => {
+    console.log('Sender', error);
+  });
 
-    await sender.connect()
-        .catch(e => {
-            console.error(`Connect error ${e}`, e);
-        });
+  await sender.connect().catch((e) => {
+    console.error(`Connect error ${e}`, e);
+  });
 
-    setInterval(async function () {
-        try {
-            const r = await sender.send(createEvent(), "AVRO_BINARY");
+  setInterval(async function () {
+    try {
+      const r = await sender.send(createEvent(), 'AVRO_BINARY');
 
-            console.log(`Status ${r.status}`)
-            if (r.status !== 204) {
-                console.error(`An error occurred while sending event:`, r)
-            }
-        } catch (e) {
-            console.error(`Error: ${e.message}`, e);
-        }
-    }, 100);
+      console.log(`Status ${r.status}`);
+      if (r.status !== 204) {
+        console.error(`An error occurred while sending event:`, r);
+      }
+    } catch (e) {
+      console.error(`Error: ${e.message}`, e);
+    }
+  }, 100);
 };
 
 const createEvent = () => {
-    const EVENT = new ClickstreamEvent();
-    EVENT.abTests = ["abc"];
-    EVENT.eventType = "button x clicked";
-    EVENT.customer = { id: "customer-id-" }; // + Math.round(Math.random() * 1000)
-    EVENT.referrer = "https://www.streammachine.io";
-    EVENT.userAgent = "node-js";
-    EVENT.producerSessionId = "session-id-"; // + Math.round(Math.random() * 100000)
-    EVENT.conversion = 1;
-    EVENT.url = "https://portal.streammachine.io/";
-    EVENT.strmMeta = {
-        eventContractRef: 'streammachine/clickstream/0.3.0',
-        // the other fields are filled in by the Stream Machine Client
-        consentLevels: [0, 1, 2]
-    };
-    return EVENT;
+  const event = new DemoEvent();
+  event.strmMeta = {
+    eventContractRef: 'streammachine/example/1.2.3',
+    consentLevels: [0],
+  };
+
+  event.unique_identifier = 'string';
+  event.some_sensitive_value = 'A value that should be encrypted';
+  event.consistent_value = 'a-user-session';
+  event.not_sensitive_value = 'Hello from NodeJS';
+
+  return event;
 };
 
-const EVENT = createEvent();
-
 startSender();
-
-
